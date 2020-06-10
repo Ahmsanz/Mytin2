8752350ed5b61060f3f04c06382286f14e62f2d2
ahmsanz <adrian.h.m.sanz@gmail.com>
2020-06-10 11:42:59 +0200

    adding build folder

---
+++ client/build/precache-manifest.facb0d42f6b3f575ab29103616f43f93.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adafe5a0abc92b4b189b1d36d2ac4b84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aa7ce3d2cf2137e21aa",
+    "url": "/static/css/main.acab0586.chunk.css"
+  },
+  {
+    "revision": "e482934c12f05b425217",
+    "url": "/static/js/2.84fe27ef.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/static/js/2.84fe27ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aa7ce3d2cf2137e21aa",
+    "url": "/static/js/main.f62fa704.chunk.js"
+  },
+  {
+    "revision": "6b127c617e1ff7457f7f",
+    "url": "/static/js/runtime-main.adb1e08b.js"
+  },
+  {
+    "revision": "52b2e4f9452f789c8b26279d471ea2a0",
+    "url": "/static/media/Chulapa-Regular.52b2e4f9.otf"
+  },
+  {
+    "revision": "f0d6313e6c1d3b48d892bb7a4b639670",
+    "url": "/static/media/DMMono-Regular.f0d6313e.ttf"
+  },
+  {
+    "revision": "a227b9b34c73ccb652b542ab0b57d717",
+    "url": "/static/media/MYtineraryLogo.a227b9b3.png"
+  }
+]);
